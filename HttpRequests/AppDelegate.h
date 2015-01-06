6d15ceffff9d2fe24413a438bafb1012007cfda5
James Stiehl <jstiehl@gs-mbp15-james.local>
2015-01-05 19:03:21 -0800

    Initial Commit

---
+++ HttpRequests/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HttpRequests
+//
+//  Created by James Stiehl on 1/5/15.
+//  Copyright (c) 2015 James Stiehl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HttpRequests/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HttpRequests
+//
+//  Created by James Stiehl on 1/5/15.
+//  Copyright (c) 2015 James Stiehl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
